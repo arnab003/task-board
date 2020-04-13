191694ffcb2c42c00d63afa425640d30a09dd24f
arnab003 <arnabphoto@gmail.com>
2020-04-14 04:14:26 +0530

    Updates

---
+++ precache-manifest.16f584bceb69ec49c2255a11fb0ca4f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a8ae4e50191fe165ecd7e64b3454a32",
+    "url": "/task-board/index.html"
+  },
+  {
+    "revision": "4d7519d05bdc19f92da8",
+    "url": "/task-board/static/css/2.36020323.chunk.css"
+  },
+  {
+    "revision": "8fbedc0147a9da3465c4",
+    "url": "/task-board/static/css/main.6a794949.chunk.css"
+  },
+  {
+    "revision": "4d7519d05bdc19f92da8",
+    "url": "/task-board/static/js/2.84d4c8d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/task-board/static/js/2.84d4c8d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fbedc0147a9da3465c4",
+    "url": "/task-board/static/js/main.1a9522a0.chunk.js"
+  },
+  {
+    "revision": "7b5bd8e736d9e4cf5ef5",
+    "url": "/task-board/static/js/runtime-main.46fd4dd7.js"
+  }
+]);
